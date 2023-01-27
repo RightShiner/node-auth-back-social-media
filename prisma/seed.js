9939f11199e6ce2518a342109b87e0010496fbb4
RightShiner <brilight.shiner@gmail.com>
2023-01-27 23:32:30 +0100

    feat:migrate1

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,13 +2,11 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  const asdf = await prisma.users.upsert({
-    where: { email: "ty@mail.com" },
-    update: {},
-    create: {
+  const user = await prisma.users.create({
+    data: {
       email: "ty@mail.com",
       name: "asdf",
-      password: "$10$rwH1Wul/1yyp133t3VzHVe4a2LeAszOzZVrghVSZfSboUyqbhnS5C",
+      password: "$2y10$rwH1Wul/1yyp133t3VzHVe4a2LeAszOzZVrghVSZfSboUyqbhnS5C",
       role_id: 1,
       phone_no: "123456",
       profile_pic: "1674847646809.png",
